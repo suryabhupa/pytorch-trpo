5ab3bbfcf6a7baca30941f04ef104de731b6f0fa
Surya Bhupatiraju <surya95@gmail.com>
2017-12-20 19:03:07 +0000

    update

--- run_i3.sh
+++ run_i3.sh
@@ -1,12 +1,5 @@
-python main.py --env-name "Reacher-v1" --eval-grad --eval-grad-gae --eval-grad-freq 10 --seed 10 &
-python main.py --env-name "Reacher-v1" --eval-grad --eval-grad-gae --eval-grad-freq 10 --seed 20 &
-python main.py --env-name "Reacher-v1" --eval-grad --eval-grad-gae --eval-grad-freq 10 --seed 30 &
-python main.py --env-name "Reacher-v1" --eval-grad --eval-grad-gae --eval-grad-freq 10 --seed 40 &
-python main.py --env-name "Reacher-v1" --eval-grad --eval-grad-gae --eval-grad-freq 10 --seed 50 &
-
-python main.py --env-name "Reacher-v1" --eval-grad --eval-grad-qe --eval-grad-freq 10 --seed 10 &
-python main.py --env-name "Reacher-v1" --eval-grad --eval-grad-qe --eval-grad-freq 10 --seed 20 &
-python main.py --env-name "Reacher-v1" --eval-grad --eval-grad-qe --eval-grad-freq 10 --seed 30 &
-python main.py --env-name "Reacher-v1" --eval-grad --eval-grad-qe --eval-grad-freq 10 --seed 40 &
-python main.py --env-name "Reacher-v1" --eval-grad --eval-grad-qe --eval-grad-freq 10 --seed 50 &
-
+python main.py --env-name "Reacher-v1" --eval-grad-gae --seed 10 &
+python main.py --env-name "Reacher-v1" --eval-grad-gae --seed 20 &
+python main.py --env-name "Reacher-v1" --eval-grad-gae --seed 30 &
+python main.py --env-name "Reacher-v1" --eval-grad-gae --seed 40 &
+python main.py --env-name "Reacher-v1" --eval-grad-gae --seed 50 & 
