7f88a9ccc2ffb8be8af2308f3d6e2619ad3397d2
Surya Bhupatiraju <surya95@gmail.com>
2018-01-10 18:34:44 +0000

    push results

--- run_i10.sh
+++ run_i10.sh
@@ -1,6 +1,4 @@
-python main.py --env-name 'Ant-v1' --eval-grad-fqe --seed 100 --l2 1e-3 &
-python main.py --env-name 'Ant-v1' --eval-grad-fqe --seed 200 --l2 1e-3 &
-python main.py --env-name 'Ant-v1' --eval-grad-fqe --seed 300 --l2 1e-3 &
-python main.py --env-name 'Ant-v1' --eval-grad-fqe --seed 400 --l2 1e-3 &
-python main.py --env-name 'Ant-v1' --eval-grad-fqe --seed 500 --l2 1e-3 &
+python main.py --env-name "HalfCheetah-v1" --eval-grad-fqe --seed 123 &
+python main.py --env-name "HalfCheetah-v1" --eval-grad-fqe --seed 124 &
+python main.py --env-name "HalfCheetah-v1" --eval-grad-fqe --seed 125 &
 
