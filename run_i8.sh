a68570bc1db93d99c5eab9444dcb7751494414da
Surya Bhupatiraju <surya95@gmail.com>
2018-01-10 18:32:45 +0000

    push results

--- run_i8.sh
+++ run_i8.sh
@@ -1,5 +1,3 @@
-python main.py --env-name 'Ant-v1' --eval-grad-fqe --seed 100 --lr 1e-2 &
-python main.py --env-name 'Ant-v1' --eval-grad-fqe --seed 200 --lr 1e-2 &
-python main.py --env-name 'Ant-v1' --eval-grad-fqe --seed 300 --lr 1e-2 &
-python main.py --env-name 'Ant-v1' --eval-grad-fqe --seed 400 --lr 1e-2 &
-python main.py --env-name 'Ant-v1' --eval-grad-fqe --seed 500 --lr 1e-2 &
+python main.py --env-name "HalfCheetah-v1" --anneal-gamma 0.75 --eval-grad-fqe --seed 123 &
+python main.py --env-name "HalfCheetah-v1" --anneal-gamma 0.75 --eval-grad-fqe --seed 124 &
+python main.py --env-name "HalfCheetah-v1" --anneal-gamma 0.75 --eval-grad-fqe --seed 125 &
